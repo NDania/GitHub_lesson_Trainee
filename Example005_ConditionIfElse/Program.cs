e7d053babc23f1c2ac8808e26c63e364b2204892
Dania <Dania2008@list.ru>
2022-09-16 19:56:30 +0300

    Added If and Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dania")
+{
+    Console.WriteLine("Привет, красотка!");
+}
+else
+{
+    Console.Write("Привет, дурачок! ");
+    Console.WriteLine(username);
+}
